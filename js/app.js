6d369fddc59e70e071379a6005c331f643e8244d
ganchentsang <ganchentsangt@gmail.com>
2022-02-01 21:30:29 -0500

    complete constant and initialized variables

--- js/app.js
+++ js/app.js
@@ -1,16 +1,31 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
+// initialized the variable winCondition to list all the different win conditions
+const winCondition = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+/// initialized the variables board, turn, and winner
+let board, turn, winner
 
 /*------------------------ Cached Element References ------------------------*/
 
+const squares = document.querySelectorAll('.square')
+const message = document.querySelector('#message')
 
+const resetButton = document.getElementById('reset-button')
 
 /*----------------------------- Event Listeners -----------------------------*/
+
 
 
 
