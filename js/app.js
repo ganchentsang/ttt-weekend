f2c1c15bda56752261ea7eb7c42fa8255a4313a4
ganchentsang <ganchentsangt@gmail.com>
2022-02-06 17:47:42 -0500

    complete lab

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*-------------------------------- Constants --------------------------------*/
 
-// initialized the variable winCondition to list all the different win conditions
-const winCondition = [
+// initialized the constant winConditions to label all the different variation of how to win
+const winConditions = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -9,54 +9,111 @@ const winCondition = [
   [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
-  [2, 4, 6]
-]
+  [2, 4, 6],
+];
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-/// initialized the variables board, turn, and winner
-let board, turn, winner
+// intialized the variables board, playerTurn, and isWinner
+let board, playerTurn, isWinner;
 
 /*------------------------ Cached Element References ------------------------*/
 
-// initialized 'sqaures' which stores the 9 different elements that make up the game
-const squares = document.querySelectorAll('.square')
-// intialized 'message' which displays the game status
-const message = document.querySelector('#message')
+// initalized element references for the previously initialized variables
+const squares = document.querySelectorAll(".squares");
+const msgToPlayer = document.querySelector("#message");
+const resetGame = document.querySelector("#reset-button");
+
+// initialized the function handleClick to handle when the board is clicked
+function handleClick(sec) {
+  resetGame.removeAttribute("hidden");
+  let sqIdx = parseInt(sec.target.id.replace("sq", ""));
+
+  if (board[sqIdx] || isWinner) {
+    return;
+  }
+
+  board[sqIdx] = playerTurn;
+
+  playerTurn *= -1;
+
+  isWinner = winnerIsYou();
+
+  render();
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
 
+// intialized an event listener to call handleClick when a cell is clicked
+squares.forEach((cell) => {
+  cell.addEventListener("click", handleClick);
+});
 
+resetGame.addEventListener("click", init);
 
 /*-------------------------------- Functions --------------------------------*/
 
-// called the initialize function
-init ()
+// called the init function
+init();
 
-// created the init function and added instructions
+// initialized the init function and called the render function
 function init() {
-  // inserted different numbers into the starting variables
-  board = [null, null, null, null, null, null, null, null, null]
-  turn = 1
-  winner = null
-  // called the render function
-  render()
+  resetGame.setAttribute("hidden", "");
+  board = [null, null, null, null, null, null, null, null, null];
+  squares.innerHTML = "";
+  isWinner = false;
+  playerTurn = 1;
+  squares.forEach((sec) => {
+    sec.innerText = "";
+  });
+  render();
 }
 
-// initialized the render function and added instructions
+// initialized the render function
 function render() {
-  // added a forEach method to change the colors of the cells when clicked
-  board.forEach((cell, idx) => {
-    let cellColor
-    if (cell === 1) {
-      cellColor == 'green'
+  board.forEach(function (section, idx) {
+    if (section === 1) {
+      squares[idx].innerText = "X";
+      squares[idx].style.backgroundColor = "cyan";
+    } else if (section === -1) {
+      squares[idx].innerText = "O";
+      squares[idx].style.backgroundColor = "magenta";
+    } else if (section === null) {
+      squares[idx].style.backgroundColor = "white";
     }
-    else if (cell === -1) {
-      cellColor = 'red'
+  });
+
+  // created an if else loop to basically decide if the game keeps on going or if it stops
+  if (!isWinner) {
+    if (playerTurn === 1) {
+      msgToPlayer.innerText = "Player X go!";
+    } else {
+      msgToPlayer.innerText = "Player O go!";
     }
-    else if (cell === null) {
-      cellColor = 'white'
+  } else if (isWinner === "T") {
+    msgToPlayer.innerText = "Tie game!";
+  } else {
+    if (isWinner === 1) {
+      msgToPlayer.innerText = "Player X wins!";
+    } else {
+      msgToPlayer.innerText = "Player O wins!";
     }
-    squares[idx].getElementsByClassName.background = cellColor
-  })
+  }
+}
+
+// initialized the function winnerIsYou return who the winner is
+function winnerIsYou() {
+  if (Math.abs(board[0] + board[1] + board[2]) === 3) return board[0];
+  if (Math.abs(board[3] + board[4] + board[5]) === 3) return board[3];
+  if (Math.abs(board[6] + board[7] + board[8]) === 3) return board[6];
+  if (Math.abs(board[0] + board[3] + board[6]) === 3) return board[0];
+  if (Math.abs(board[1] + board[4] + board[7]) === 3) return board[1];
+  if (Math.abs(board[2] + board[5] + board[8]) === 3) return board[2];
+  if (Math.abs(board[0] + board[4] + board[8]) === 3) return board[0];
+  if (Math.abs(board[2] + board[4] + board[6]) === 3) return board[2];
+  if (board.includes(null)) {
+    return null;
+  } else {
+    return "T";
+  }
 }
